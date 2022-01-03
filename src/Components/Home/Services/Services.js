0d964bbaab27103612440af537338b04748c648e
unknown <apuemdad96@gmail.com>
2022-01-03 20:32:41 +0600

    responsiveness fixed

--- src/Components/Home/Services/Services.js
+++ src/Components/Home/Services/Services.js
@@ -19,7 +19,7 @@ const Services = () => {
     <div id="services" className="container">
       <h1 className="text-primary">Our Popular Bikes </h1>
       {loading && <Spinner animation="border" variant="danger" />}
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+      <div className=" row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
         {services.map((service) => (
           <Service service={service} key={service._id}></Service>
         ))}
